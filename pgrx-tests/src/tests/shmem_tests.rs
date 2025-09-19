b5aee93d7096090471988a27c5363b9f2d75a12c
Francesco Komauli <francesco.komauli@optionfactory.net>
2025-09-19 16:43:53 +0200

    :bug: using aligned shmem for dsm and scan lock tests

--- pgrx-tests/src/tests/shmem_tests.rs
+++ pgrx-tests/src/tests/shmem_tests.rs
@@ -22,9 +22,9 @@ static LWLOCK: PgLwLock<bool> = unsafe { PgLwLock::new(c"pgrx_tests_lwlock") };
 static SPINLOCK: PgAtomic<PgSpinLock<usize>> = unsafe { PgAtomic::new(c"pgrx_tests_spinlock") };
 
 static DSMLWLOCK: DsmLwLockTranche = DsmLwLockTranche::new(c"pgrx_tests_dsm_lwlock");
-static DSMLWLOCKMEM: PgAtomic<[u8; DsmLwLock::<bool>::mem_size()]> = unsafe { PgAtomic::new(c"pgrx_tests_dsm_lwlock_mem") };
+static DSMLWLOCKMEM: TestDSM = unsafe { TestDSM::new(DsmLwLock::<bool>::mem_size(), c"pgrx_tests_dsm_lwlock_mem") };
 static SCANLWLOCK: ParallelScanLwLockTranche = ParallelScanLwLockTranche::new(c"pgrx_tests_scan_lwlock");
-static SCANLWLOCKMEM: PgAtomic<[u8; ParallelScanLwLock::<bool>::mem_size()]> = unsafe { PgAtomic::new(c"pgrx_tests_dsm_lwlock_mem") };
+static SCANLWLOCKMEM: TestDSM = unsafe { TestDSM::new(ParallelScanLwLock::<bool>::mem_size(), c"pgrx_tests_scan_lwlock_mem") };
 
 #[pg_guard]
 pub extern "C-unwind" fn _PG_init() {
@@ -40,6 +40,55 @@ pub extern "C-unwind" fn _PG_init() {
     pg_shmem_init!(SCANLWLOCK);
     pg_shmem_init!(SCANLWLOCKMEM);
 }
+
+// Allocates just plain shared memory.
+// TODO: Should be easier by using GetNamedDSMSegment when its bindings are included.
+struct TestDSM {
+    size: usize,
+    name: &'static std::ffi::CStr,
+    inner: std::cell::UnsafeCell<*mut std::ffi::c_void>,
+}
+
+impl TestDSM {
+    pub const unsafe fn new(size: usize, name: &'static std::ffi::CStr) -> Self {
+        Self { size, name, inner: std::cell::UnsafeCell::new(std::ptr::null_mut()) }
+    }
+
+    pub unsafe fn mem(&self) -> *mut std::ffi::c_void {
+        *(self.inner.get())
+    }
+}
+
+unsafe impl Sync for TestDSM {}
+
+impl pgrx::PgSharedMemoryInitialization for TestDSM {
+    type Value = ();
+
+    unsafe fn on_shmem_request(&'static self) {
+        unsafe {
+            pgrx::pg_sys::RequestAddinShmemSpace(self.size);
+        }
+    }
+
+    unsafe fn on_shmem_startup(&'static self, _value: ()) {
+        unsafe {
+            use pgrx::pg_sys;
+
+            let shm_name = self.name;
+            let addin_shmem_init_lock = &raw mut (*pg_sys::MainLWLockArray.add(21)).lock;
+            pg_sys::LWLockAcquire(addin_shmem_init_lock, pg_sys::LWLockMode::LW_EXCLUSIVE);
+
+            let mut found = false;
+            let fv_shmem = pg_sys::ShmemInitStruct(shm_name.as_ptr(), self.size, &mut found);
+            assert!(fv_shmem.is_aligned(), "shared memory is not aligned");
+
+            *self.inner.get() = fv_shmem;
+
+            pg_sys::LWLockRelease(addin_shmem_init_lock);
+        }
+    }
+}
+
 #[cfg(any(test, feature = "pg_test"))]
 #[pgrx::pg_schema]
 mod tests {
@@ -47,7 +96,6 @@ mod tests {
     use crate as pgrx_tests;
 
     use pgrx::prelude::*;
-    use std::ffi::c_void;
     use pgrx::lwlock::dsm::DsmLwLockHandle;
     use pgrx::lwlock::scan::ParallelScanLwLock;
 
@@ -94,9 +142,9 @@ mod tests {
 
     fn init_dsm_lwlock() -> DsmLwLockHandle<bool> {
         use super::{DSMLWLOCK, DSMLWLOCKMEM};
-        let shmem = DSMLWLOCKMEM.get() as *const [u8] as *mut [u8] as *mut c_void;
         let data: bool = false;
         unsafe {
+            let shmem = DSMLWLOCKMEM.mem();
             DSMLWLOCK.init(shmem, &data as *const bool);
             DSMLWLOCK.register(shmem)
         }
@@ -132,10 +180,9 @@ mod tests {
 
     fn init_scan_lwlock() -> ParallelScanLwLock<bool> {
         use super::{SCANLWLOCK, SCANLWLOCKMEM};
-        let shmem = SCANLWLOCKMEM.get() as *const [u8] as *mut [u8] as *mut c_void;
         let data: bool = false;
         let mut lock = SCANLWLOCK.lock_for(data);
-        lock.initialize_dsm_and_register_leader(shmem);
+        unsafe { lock.initialize_dsm_and_register_leader(SCANLWLOCKMEM.mem()); }
         lock
     }
 
